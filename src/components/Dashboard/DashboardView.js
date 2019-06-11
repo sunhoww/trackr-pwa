e7b12afdcff711fc1950e78c3b64518ba71c804e
Sun Howwrongbum <sun@libermatic.com>
2019-06-11 22:32:09 +0530

    add: stub for Dashboard

---
+++ src/components/Dashboard/DashboardView.js
@@ -0,0 +1,14 @@
+// @flow
+
+import React from 'react';
+
+import SignOut from '../SignOut';
+
+export default function DashboardView() {
+  return (
+    <div>
+      <h1>DashboardView</h1>
+      <SignOut />
+    </div>
+  );
+}
