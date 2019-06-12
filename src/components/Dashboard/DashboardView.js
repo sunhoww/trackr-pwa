b8e174e951b8cd7e33c5cb28da978e31e279bce0
Sun Howwrongbum <sun@libermatic.com>
2019-06-13 00:51:42 +0530

    update: Dashboard with proper

--- src/components/Dashboard/DashboardView.js
+++ src/components/Dashboard/DashboardView.js
@@ -1,14 +1,156 @@
 // @flow
 
-import React from 'react';
+import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Hidden,
+  Drawer,
+  Container,
+  Divider,
+  useMediaQuery,
+} from '@material-ui/core';
+import {
+  Menu as MenuIcon,
+  ChevronLeft as ChevronLeftIcon,
+} from '@material-ui/icons';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import clsx from 'clsx';
 
-import SignOut from '../SignOut';
+import Profile from '../Profile';
+import Navigation from '../Navigation';
+import NotFound from '../NotFound';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Object) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+  },
+  appBarShift: {
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  main: {
+    flexGrow: 1,
+    [theme.breakpoints.up('sm')]: {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: -drawerWidth,
+    },
+  },
+  mainShift: {
+    [theme.breakpoints.up('sm')]: {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    },
+  },
+  toolbarOffset: theme.mixins.toolbar,
+  drawer: {
+    flexShrink: 0,
+    width: `calc(100vw - ${theme.spacing(12)}px)`,
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+    },
+  },
+  drawerPaper: {
+    width: `calc(100vw - ${theme.spacing(12)}px)`,
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+    },
+  },
+}));
 
 export default function DashboardView() {
+  const classes = useStyles();
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const theme = useTheme();
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up('sm'));
+
   return (
-    <div>
-      <h1>DashboardView</h1>
-      <SignOut />
+    <div className={classes.root}>
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: drawerOpen,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            className={clsx(classes.menuButton, {
+              [classes.hide]: isLargeScreen && drawerOpen,
+            })}
+            color="inherit"
+            aria-label="Open drawer"
+            edge="start"
+            onClick={() => setDrawerOpen(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          className={classes.drawer}
+          classes={{ paper: classes.drawerPaper }}
+          variant={isLargeScreen ? 'persistent' : 'temporary'}
+          open={drawerOpen}
+          onClose={() => setDrawerOpen(false)}
+          ModalProps={{ keepMounted: true }}
+        >
+          <Profile
+            action={
+              isLargeScreen && (
+                <IconButton onClick={() => setDrawerOpen(false)}>
+                  <ChevronLeftIcon />
+                </IconButton>
+              )
+            }
+          />
+          <Divider />
+          <Navigation onSelect={() => !isLargeScreen && setDrawerOpen(false)} />
+        </Drawer>
+      </nav>
+      <Container
+        component="main"
+        className={clsx(classes.main, {
+          [classes.mainShift]: drawerOpen,
+        })}
+      >
+        <div className={classes.toolbarOffset} />
+        <Route component={NotFound} />
+      </Container>
     </div>
   );
 }
