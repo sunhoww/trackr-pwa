dfcba5f0a2b49c50f18d72665cefcbb325068f0f
Sun Howwrongbum <sun@libermatic.com>
2019-06-13 00:52:24 +0530

    fix: graphql resolvers not setup

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -2,10 +2,12 @@ import { mergeDeepRight } from 'ramda';
 import { typeDef as Auth, resolvers as authResolvers } from './auth';
 
 export const typeDefs = [Auth];
+
 export const defaults = {
   isAuthed: false,
   isManualAuth: false,
   authCompleted: false,
   traccarSessionId: null,
 };
-export const resolvers = mergeDeepRight(authResolvers);
+
+export const resolvers = mergeDeepRight({}, authResolvers);
