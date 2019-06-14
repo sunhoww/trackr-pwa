508977d7996884c43c487bba081c16cf33d493f3
Sun Howwrongbum <sun@libermatic.com>
2019-06-14 20:23:06 +0530

    add: AuthenticationContext

---
+++ src/components/App/AuthenticationContext.js
@@ -0,0 +1,45 @@
+// @flow
+
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import type { Node } from 'react';
+
+import { useFirebase } from '../../services/firebase';
+
+type Context = {
+  user?: Object,
+  isSigningIn: boolean,
+};
+
+const initValues = { user: null, isSigningIn: true };
+
+const AuthenticationContext = createContext<Context>(initValues);
+
+type Props = {
+  children: Node,
+};
+
+export function AuthenticationProvider({ children }: Props) {
+  const [values, setValues] = useState(initValues);
+  const firebase = useFirebase();
+  console.log(firebase);
+  useEffect(
+    () => {
+      if (firebase) {
+        return firebase.auth.onAuthStateChanged(async user => {
+          setValues({ user, isSigningIn: false });
+        });
+      }
+    },
+    [firebase, values.user]
+  );
+
+  return (
+    <AuthenticationContext.Provider value={values}>
+      {children}
+    </AuthenticationContext.Provider>
+  );
+}
+
+export function useAuthentication() {
+  return useContext(AuthenticationContext);
+}
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,12 +1,8 @@
 // @flow
 
-import * as React from 'react';
+import { createContext, useContext } from 'react';
 import app from 'firebase/app';
 import 'firebase/auth';
-import gql from 'graphql-tag';
-
-import client from './apollo';
-import { SESSION, CREATE_SESSION } from '../graphql/queries';
 
 const firebaseConfig = JSON.parse(
   process.env.REACT_APP_FIREBASE_CONFIG || '{}'
@@ -20,27 +16,12 @@ class Firebase {
   }
 }
 
-export const FirebaseContext = React.createContext<?Firebase>(null);
+const FirebaseContext = createContext<?Firebase>(null);
 
 export const FirebaseProvider = FirebaseContext.Provider;
 
-// firebase.auth().onAuthStateChanged(async user => {
-//   if (user) {
-//     const { isManualAuth } = await client.readQuery({
-//       query: gql(`{ isManualAuth @client }`),
-//     });
-//     if (!isManualAuth) {
-//       const {
-//         data: { createSession },
-//       } = await client.mutate({ mutation: CREATE_SESSION });
-//       const { traccarSessionId, me } = createSession || {};
-//       client.writeData({ data: { isAuthed: true, traccarSessionId } });
-//       client.writeQuery({ query: SESSION, data: { me } });
-//     }
-//   } else {
-//     await client.resetStore();
-//   }
-//   client.writeData({ data: { authCompleted: true } });
-// });
+export function useFirebase() {
+  return useContext(FirebaseContext);
+}
 
 export default Firebase;
